5ac15c86e8526ed0f4af321e422857002648f79d
wedad abd elkreem <wedadabdelkreem@gmail.com>
2019-02-25 23:32:36 +0200

    update task with required comments

---
+++ app/src/main/java/com/example/wedadabdelkareem/linkdevelopmenttask/view/ui/BaseActivity.java
@@ -0,0 +1,47 @@
+package com.example.wedadabdelkareem.linkdevelopmenttask.view.ui;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.wedadabdelkareem.linkdevelopmenttask.R;
+
+public class BaseActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    @Override
+    public void setContentView(final int layoutResID) {
+        super.setContentView(layoutResID);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public Toolbar getToolbar() {
+        return toolbar;
+    }
+}
